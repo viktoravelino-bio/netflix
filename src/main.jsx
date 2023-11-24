a8da133c5392cb89ef1942be3d656c356eafef73
d4cho <danielcho91@gmail.com>
2023-11-24 12:22:32 -0500

    add bio widget

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import { VideoPortalContainer } from './components/organisms/videoPortalContainer/VideoPortalContainer';
+import { VideoPortalContainer } from "./components/organisms/videoPortalContainer/VideoPortalContainer";
 
-import { PortalProvider } from './providers/Portal';
-import { AppRouter } from './routes';
+import { PortalProvider } from "./providers/Portal";
+import { AppRouter } from "./routes";
 
-import './styles/global.css';
+import "./styles/global.css";
+import BioStorybookWidget from "bio-storybook-widget";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BioStorybookWidget projectSource={"netflix"} />
     <BrowserRouter>
       <PortalProvider>
         <AppRouter />
